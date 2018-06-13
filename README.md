3b57d272e615003061c7018d05e92191f0fdfb6a
Petr Havelka <petr.havelka@unicornsystems.eu>
2018-06-13 23:13:20 +0200

    Fixed bugs - language switch is working, edit is editing correct section New features - ESC key, Ctrl+Enter key -

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ It will add useful links into page.
 - pencil icon for edit document structure is added (if used has permissions)
 - MD link for edit document in [uuDockitHelper](https://github.com/jiridudekusy/uuDockitHelper) is added (if used has permissions)
 - pencil icon for edit each section is added (if user has permission)
+- ESC key will close edit window
+- Ctrl+Enter key will save edit window 
 
 # Install as a User Script - Tampermonkey
 1. Add https://tampermonkey.net/ into your browser
@@ -11,18 +13,3 @@ It will add useful links into page.
 
 # Configuration
 - In case of [uuDockitHelper](https://github.com/jiridudekusy/uuDockitHelper) is executed on different IP or port than http://localhost:4323/vendor-app-subapp/0-0/editor please edit the `mdUrl` in script. 
-
-# Chrome extension for uuBookKit - depricated
-
-## Installation as developer
-1. Download this repo.
-2. Open Chrome and navigate to [chrome://extensions/](chrome://extensions/)
-3. Enable developer mode
-4. Load Unpacked extension - select uubookkit-ext folder from repo
-
-## Install from package
-Unfortunatelly this it not working because this extensions is not on published on Chrome Web Store yet. $5 payment is required.
-
-Download [uubookkit-ext.crx](uubookkit-ext.crx) and drag-and-drop it into Chrome and confim plugin installation.
-
-
--- uubookkit-ext.user.js
+++ uubookkit-ext.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         uuBookKit-ext
 // @namespace    https://github.com/PetrHavelka/uubookkit-ext
-// @version      0.3.1
+// @version      0.4.0
 // @description  Add usefull links to page
 // @author       Petr Havelka
 // @match        https://uuos9.plus4u.net/uu-dockitg01-main/*
@@ -25,9 +25,8 @@ GM_addStyle(`
   font-size: 19px;
 }
 
-.uu-bookkit-book-top-text a {
-  color: white;
-  text-decoration: none;
+.uu-bookkit-book-top-text {
+  cursor: pointer;
 }
 
 .bookkit-ext-md {
@@ -38,100 +37,154 @@ GM_addStyle(`
 }
 `);
 
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    // init of each bookkit page
-    let initPage = function() {
-        let page = $(".uu-bookkit-page");
-        // if page not ready do it later
-        if (!page.length) {
-            setTimeout(initPage, 1000);
-        }
-        // page is already done
-        if (page.hasClass("bookkit-ext-page-done")) return;
+  console.log("uuBookKit-ext starting...");
 
-        // update HTML - add icons and links
-        let editIcon = 'uu5-bricks-icon mdi mdi-lead-pencil';
+  let currentPageData = {};
 
-        // no rights
-        if ($(".uu-bookkit-control-bar-executives").length) {
-            $(".uu-bookkit-page h1 .uu-bookkit-page-ready-header")
-                .append('<span class="bookkit-ext-edit ' + editIcon + '" data-link="Upravit strukturu obsahu"></span>')
+  // init of each bookkit page
+  let initPage = function () {
+    let page = $(".uu-bookkit-page-ready");
+    // if page not ready do it later
+    if (!page.length) {
+      setTimeout(initPage, 1000);
+    }
+    // page is already done - remove all links
+    if (page.hasClass("bookkit-ext-page-done")) {
+      $(".bookkit-ext-edit").remove();
+      $(".bookkit-ext-md").remove();
+    }
 
+    // update HTML - add icons and links
+    let editIcon = 'uu5-bricks-icon mdi mdi-lead-pencil';
 
-            $(".uu-bookkit-page h2.uu5-bricks-header").each(function (i) {
-                $(this).append('<span class="bookkit-ext-edit ' + editIcon + '" data-link="Upravit obsah - sekce ' + i + '"></span>');
-            });
-        }
+    let pageTitle = $(".uu-bookkit-page h1 .uu-bookkit-page-ready-header");
+    // if i have rights for edit
+    if ($(".uu-bookkit-control-bar-executives").length) {
+      pageTitle.append('<span class="bookkit-ext-edit ' + editIcon + '" data-link="Upravit strukturu obsahu"></span>');
 
-        $(".uu-bookkit-page h1 .uu-bookkit-page-ready-header")
-            .append('<a href="' + mdUrl + '?page='+ encodeURIComponent(window.location.href) + '" target="_blank" class="bookkit-ext-md">MD</span></a>');
-
-
-        // mark page as ready
-        page.addClass("bookkit-ext-page-done");
-    };
-
-    // first init of whole webpage
-    let firstInit = function() {
-        let title = $(".uu-bookkit-book-top-text");
-        // if page not loaded yer - do it later
-        if (!title.length) {
-            setTimeout(firstInit, 3000);
-        }
-
-        // update HTML - add icons and links
-        let refreshIcon = '<span class="uu5-bricks-icon plus4u5 plus4u5-refresh bookkit-ext-refresh"></span>';
-        title.wrapInner('<a href="https://docs.plus4u.net/book"></a>');
-        title.append(refreshIcon);
-
-        // init bookkit page
-        initPage();
-    };
-
-    // first action take after 3s
-    setTimeout(firstInit, 3000);
-
-    // click on link by its name
-    let clickLinkByName = function (name) {
-        // console.log('Click link by name = "' + name + '"')
-
-        // find all linsk in controll menu
-        $('a.uu5-bricks-dropdown-item-link').each(function() {
-            let item = $(this);
-            // compare its text
-            if (item.text() == name) {
-                // mark link with #temp
-                item.attr("id", "temp");
-                // click on link
-                document.getElementById("temp").click();
-                // remove temp id
-                item.attr("id", "");
+      $(".uu-bookkit-page h2.uu5-bricks-header, .uu-bookkit-page h3.uu5-bricks-header").each(function (i) {
+        // find correct index
+        let title = $(this).text();
+        if (currentPageData && currentPageData.body) {
+          for (let a = 0; a < currentPageData.body.length; a++) {
+            if (currentPageData.body[a].includes('header="' + title + '"')) {
+              i = a;
+              break;
             }
+          }
+        }
+        $(this).append('<span class="bookkit-ext-edit ' + editIcon
+            + '" data-link="Upravit obsah - sekce ' + i
+            + '" title="Upravit obsah - sekce ' + i + '"></span>');
+      });
+    }
+
+    // add MD link
+    pageTitle.append('<a href="' + mdUrl + '?page=' + encodeURIComponent(window.location.href) + '" target="_blank" class="bookkit-ext-md">MD</span></a>');
+
+    // mark page as ready
+    page.addClass("bookkit-ext-page-done");
+  };
+
+  // first init of whole webpage
+  let firstInit = function () {
+    let title = $(".uu-bookkit-book-top-text");
+    // if page not loaded yer - do it later
+    if (!title.length) {
+      setTimeout(firstInit, 3000);
+    }
+
+    // update HTML - add icons and links
+    let refreshIcon = '<span class="uu5-bricks-icon plus4u5 plus4u5-refresh bookkit-ext-refresh"></span>';
+    title.after(refreshIcon);
+
+    // init bookkit page
+    initPage();
+  };
+
+  // inject to CMD call
+  let injectToHttpRequest = function () {
+    let origOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function (method, url, async, user, pass) {
+
+      if (url.includes("loadPage")) {
+        this.addEventListener('load', function () {
+          currentPageData = JSON.parse(this.responseText);
+          initPage();
         });
-    };
+      }
+      origOpen.apply(this, arguments);
+    }
+  };
 
-    // handle all clicks on webpage
-    $(document).click(function(e) {
-        // console.log($(e.target));
+  // do inject
+  injectToHttpRequest();
 
-        // is it click to refresh button?
-        if ($(e.target).hasClass("bookkit-ext-refresh")) {
-            // init page
-            initPage();
-            return;
-        }
+  // first action take after 3s
+  setTimeout(firstInit, 3000);
 
-        // is it click to edit button?
-        if ($(e.target).hasClass("bookkit-ext-edit")) {
-            // click to particular link in control menu
-            clickLinkByName($(e.target).data("link"));
-            return;
-        }
+  // click on element
+  let click = function(element) {
+    console.log(element);
+    // mark link with #temp
+    element.attr("id", "temp");
+    // click on link
+    document.getElementById("temp").click();
+    // remove temp id
+    element.attr("id", "");
+  };
 
-        // any other click is probably loading next page - so init it after 1s
-        setTimeout(initPage, 1000);
+  // click on link in menu by its name
+  let clickLinkByName = function (name) {
+    // console.log('Click link by name = "' + name + '"')
+
+    // find all linsk in controll menu
+    $('a.uu5-bricks-dropdown-item-link').each(function () {
+      let item = $(this);
+      // compare its text
+      if (item.text() === name) {
+        click(item);
+      }
     });
+  };
+
+  // handle all clicks on webpage
+  $(document).click(function (e) {
+    // console.log($(e.target));
+
+    // is it click to refresh button?
+    if ($(e.target).hasClass("bookkit-ext-refresh")) {
+      // init page
+      initPage();
+      return;
+    }
+
+    // is it click to edit button?
+    if ($(e.target).hasClass("bookkit-ext-edit")) {
+      // click to particular link in control menu
+      clickLinkByName($(e.target).data("link"));
+      return;
+    }
+
+    // click to page title is linking to knowledge base
+    if ($(e.target).parent().hasClass("uu-bookkit-book-top-text")) {
+      window.location.href = "https://docs.plus4u.net/book";
+    }
+  });
+
+  $(document).keyup(function (e) {
+    // close edit dialog on ESC
+    if (e.keyCode === 27) {
+      click($(".uu5-bricks-modal-l .uu5-bricks-modal-header-close"));
+    }
+
+    // save edit dialog on CTRL + ENTER
+    if (e.ctrlKey && e.keyCode === 13) {
+      click($(".uu5-bricks-modal-l .color-schema-primary.uu5-bricks-button"));
+    }
+  });
 
 })();
